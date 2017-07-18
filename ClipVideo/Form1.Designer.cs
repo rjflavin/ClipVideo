08ca93e23e809f762cf962794067fe1feea3820b
rjflavin <rjflavin@gmail.com>
2017-07-18 09:43:36 -0700

    Add files via upload

---
+++ ClipVideo/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ClipVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.createClipButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(750, 456);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(867, 12);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(872, 456);
+            this.axWindowsMediaPlayer2.TabIndex = 1;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 494);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(750, 92);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // createClipButton
+            // 
+            this.createClipButton.Location = new System.Drawing.Point(867, 494);
+            this.createClipButton.Name = "createClipButton";
+            this.createClipButton.Size = new System.Drawing.Size(872, 92);
+            this.createClipButton.TabIndex = 3;
+            this.createClipButton.Text = "Create Clip";
+            this.createClipButton.UseVisualStyleBackColor = true;
+            this.createClipButton.Click += new System.EventHandler(this.createClipButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1822, 845);
+            this.Controls.Add(this.createClipButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeEnd);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button createClipButton;
+    }
+}
+
---
+++ ClipVideo/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace ClipVideo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            IntPtr window = this.Handle;
+
+            //Attach Hook
+            attachWindowHook();
+
+
+
+            //Initial Video Setup
+            axWindowsMediaPlayer1.Width = (int)(this.Width / 2.2);
+            axWindowsMediaPlayer1.Height = (int)(this.Height / 2.2);
+            axWindowsMediaPlayer1.Location = new Point(0, 0);
+            axWindowsMediaPlayer1.URL = @"C:\Cases\SF v Downey\Gonzalez\DVD Video Recording_Title_01_01.mpg";
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+            axWindowsMediaPlayer1.settings.autoStart = false;
+
+
+            axWindowsMediaPlayer2.Location = new Point(this.Width / 2, 0);
+            axWindowsMediaPlayer2.Height = (int)(this.Height / 2.2);
+            axWindowsMediaPlayer2.Width = (int)(this.Width / 2.2);
+            axWindowsMediaPlayer2.URL = @"C:\Cases\SF v Downey\Gonzalez\DVD Video Recording_Title_01_01.mpg";
+            axWindowsMediaPlayer2.Ctlcontrols.stop();
+            axWindowsMediaPlayer2.settings.autoStart = false;
+
+            //Resize
+            Form1_ResizeEnd(null, null);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+            //axWindowsMediaPlayer1.openPlayer(axWindowsMediaPlayer1.URL);
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Width = (int)(this.ClientRectangle.Width / 2);
+            axWindowsMediaPlayer1.Height = (int)(7*this.ClientRectangle.Height / 8);
+            axWindowsMediaPlayer1.Location = new Point(0, 0);
+
+            axWindowsMediaPlayer2.Location = new Point(this.ClientRectangle.Width / 2, 0);
+            axWindowsMediaPlayer2.Height = (int)(7*this.ClientRectangle.Height / 8);
+            axWindowsMediaPlayer2.Width = (int)(this.ClientRectangle.Width / 2);
+
+            loadButton.Location = new Point(0, this.ClientRectangle.Height - this.ClientRectangle.Height / 8);
+            loadButton.Width = this.ClientRectangle.Width / 2;
+            loadButton.Height = this.ClientRectangle.Height /8;
+
+            createClipButton.Location = new Point(this.ClientRectangle.Width / 2, this.ClientRectangle.Height - this.ClientRectangle.Height / 8);
+            createClipButton.Width = this.ClientRectangle.Width / 2;
+            createClipButton.Height = this.ClientRectangle.Height / 8;
+        }
+
+
+        private void attachWindowHook()
+        {
+            if (hHook == 0)
+            {
+                // Create an instance of HookProc.
+                MouseHookProcedure = new HookProc(Form1.MouseHookProc);
+
+                hHook = SetWindowsHookEx(WH_MOUSE,
+                MouseHookProcedure,
+                (IntPtr)0,
+                AppDomain.GetCurrentThreadId());
+                //If the SetWindowsHookEx function fails.
+                if (hHook == 0)
+                {
+                    MessageBox.Show("SetWindowsHookEx Failed");
+                    return;
+                }
+            }
+            else
+            {
+                bool ret = UnhookWindowsHookEx(hHook);
+                //If the UnhookWindowsHookEx function fails.
+                if (ret == false)
+                {
+                    MessageBox.Show("UnhookWindowsHookEx Failed");
+                    return;
+                }
+                hHook = 0;
+                this.Text = "Mouse Hook";
+            }
+        }
+
+        public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        //Declare the hook handle as an int.
+        static int hHook = 0;
+
+        //Declare the mouse hook constant.
+        //For other hook types, you can obtain these values from Winuser.h in the Microsoft SDK.
+        public const int WH_MOUSE = 7;
+
+        //Declare MouseHookProcedure as a HookProc type.
+        HookProc MouseHookProcedure;
+
+        //Declare the wrapper managed POINT class.
+        [StructLayout(LayoutKind.Sequential)]
+        public class POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        //Declare the wrapper managed MouseHookStruct class.
+        [StructLayout(LayoutKind.Sequential)]
+        public class MouseHookStruct
+        {
+            public POINT pt;
+            public int hwnd;
+            public int wHitTestCode;
+            public int dwExtraInfo;
+        }
+
+        //This is the Import for the SetWindowsHookEx function.
+        //Use this function to install a thread-specific hook.
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+         CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn,
+        IntPtr hInstance, int threadId);
+
+        //This is the Import for the UnhookWindowsHookEx function.
+        //Call this function to uninstall the hook.
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+         CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        //This is the Import for the CallNextHookEx function.
+        //Use this function to pass the hook information to the next hook procedure in chain.
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+         CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode,
+        IntPtr wParam, IntPtr lParam);
+
+        public static int MouseHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            //Marshall the data from the callback.
+            MouseHookStruct MyMouseHookStruct = (MouseHookStruct)Marshal.PtrToStructure(lParam, typeof(MouseHookStruct));
+
+            if (nCode < 0)
+            {
+                return CallNextHookEx(hHook, nCode, wParam, lParam);
+            }
+            else
+            {
+                //Create a string variable that shows the current mouse coordinates.
+                String strCaption = "x = " +
+                MyMouseHookStruct.pt.x.ToString("d") +
+                "  y = " +
+                MyMouseHookStruct.pt.y.ToString("d");
+                //You must get the active form because it is a static function.
+                //Form tempForm = Form.ActiveForm;
+
+                //Set the caption of the form.
+                //tempForm.Text = strCaption;
+                return CallNextHookEx(hHook, nCode, wParam, lParam);
+            }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.ShowDialog();
+
+            if (ofd.CheckFileExists)
+            {
+                axWindowsMediaPlayer1.URL = ofd.FileName;
+                axWindowsMediaPlayer2.URL = ofd.FileName;
+
+            }
+        }
+
+        private void createClipButton_Click(object sender, EventArgs e)
+        {
+            String startTime = axWindowsMediaPlayer1.Ctlcontrols.currentPosition.ToString();
+            String endTime = axWindowsMediaPlayer2.Ctlcontrols.currentPosition.ToString();
+            MessageBox.Show("EndTime:" + axWindowsMediaPlayer1.Ctlcontrols.currentMarker.ToString());
+            
+
+            String runTime = (axWindowsMediaPlayer2.Ctlcontrols.currentPosition - axWindowsMediaPlayer1.Ctlcontrols.currentPosition).ToString();
+            String inputFile = axWindowsMediaPlayer1.URL;
+            String folder = "output";
+
+            TimeSpan startTimespan = TimeSpan.FromSeconds(Convert.ToDouble(startTime));
+            TimeSpan runTimespan = TimeSpan.FromSeconds(Convert.ToDouble(runTime));
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            if (!(startTimespan.TotalSeconds > Convert.ToDouble(endTime)))
+            {
+
+
+                string command = "ffmpeg -i \"" + inputFile + "\" -ss " + startTimespan.ToString().Substring(0, startTimespan.ToString().Length - 4) + " -to " + TimeSpan.FromSeconds(Convert.ToDouble(endTime)) + " " + "\"" + axWindowsMediaPlayer1.URL.ToString().Split('\\')[axWindowsMediaPlayer1.URL.Split('\\').Length - 1].Split('.')[0] + "_" + startTime.ToString().Replace('.', '-') + "_" + endTime.ToString().Replace('.', '-') + "." + axWindowsMediaPlayer1.URL.ToString().Split('\\')[axWindowsMediaPlayer1.URL.Split('\\').Length - 1].Split('.')[1] + "\"";
+                Process.Start("CMD.exe", "/K echo " + command + "&" + command);
+            }
+            else
+            {
+                MessageBox.Show("Clip start time comes before clip end time");
+            }
+        }
+    }
+
+
+}
